918dc68da7e5c811fe5af286a7ec44f55e4d4ba0
jhonny919 <56691133+jhonny919@users.noreply.github.com>
2020-09-22 21:19:39 +0300

    Add files via upload

---
+++ 4kyu Remove Zeros.js
@@ -0,0 +1,10 @@
+function removeZeros(array) {
+  
+  let arr=[],zero=[];
+  for(let i=0;i<array.length;i++){
+    if(array[i]===0 || array[i]==="0")zero[zero.length]=array[i];
+    else arr[arr.length]=array[i];
+  }
+  
+  return [...arr,...zero];
+}
